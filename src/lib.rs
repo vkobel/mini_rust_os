bdce3bc0f49b9e0848c9ee2bf5234178992c30a7
Vincent Kobel <vincent.kobel@nagra.com>
2016-04-25 16:10:38 +0200

    hello world in rust

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub extern fn rust_main() {
     // ATTENTION: we have a very small stack and no guard page
 
     let hello = b"Hello World!";
-    let color_byte = 0x1f; // white foreground, blue background
+    let color_byte = 0x6f; // white foreground, blue background
 
     let mut hello_colored = [color_byte; 24];
     for (i, char_byte) in hello.into_iter().enumerate() {
